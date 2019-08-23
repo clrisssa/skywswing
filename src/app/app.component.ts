e6745511b81bd2be8ee8ee8702a7bac889f58949
clrisssa <its.clarissa@gmail.com>
2019-08-23 15:21:50 +0700

    added path tracking for GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators'
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -25,7 +28,18 @@ export class AppComponent {
     }
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    const navEndEvents$ = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-106185727-3', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
 
   menuOnClick(menuItem) {
     // console.log("menuonclick:" + menuItem)
